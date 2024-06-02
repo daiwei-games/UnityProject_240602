0b3844bff35be5640d0914b32019f46a4b2867da
daiwei-games <98375826+daiwei-games@users.noreply.github.com>
2024-06-02 16:38:29 +0800

    重新上傳

--- Project/Assets/Scripts/AudioObjectList.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Audio ListObject", menuName = "object/Audio/Add New ListObject")]
-public class AudioObjectList : ScriptableObject
-{
-    public List<AudioClip> clipList;
-}
--- Project/README.md
+++
@@ -1,2 +0,0 @@
-# Unity_YoloToy_DinosaurGame_20240422
- Unity 有樂玩具 恐龍遊戲2024.04.22
